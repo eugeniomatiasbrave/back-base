48b81a2d3bdda370e5d4b7c676991e5081740c70
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2025-03-25 09:50:23 -0300

    middlewere Validator inputs (register,login,product)

--- src/router/ProductsRouter.js
+++ src/router/ProductsRouter.js
@@ -1,12 +1,13 @@
 import BaseRouter from "./BaseRouter.js";
 import productController from "../controllers/products.controller.js";
+import { productValidator } from "../middlewares/validate.js"; // Importa el middleware de validación
 
 class ProductsRouter extends BaseRouter {
 	
 	init(){
 		this.get('/', ['PUBLIC','USER'], productController.getProducts);
         this.get('/:id', ['PUBLIC','USER'], productController.getProductById); // podra tener respuesta el publico como  aquellos con rol user.
-        this.post('/', ['PUBLIC'], productController.createProduct);
+        this.post('/', ['PUBLIC'], productValidator, productController.createProduct);
         this.put('/:id', ['ADMIN'], productController.updateProduct);
         this.delete('/:id', ['ADMIN'], productController.deleteProduct);
 	}
--- src/router/UsersRouter.js
+++ src/router/UsersRouter.js
@@ -1,14 +1,13 @@
 import BaseRouter from "./BaseRouter.js";
 import usersController from "../controllers/users.controller.js";
 
-
 class UsersRouter extends BaseRouter { // creo una clase llamada UsersRouter que extiende de BaseRouter
 
 	init(){
 		this.get('/', ['PUBLIC'], usersController.getUsers); // GET /api/users/
         this.get('/:id', ['PUBLIC'], usersController.getUserById); // GET /api/users/:id
         this.post('/', ['PUBLIC'], usersController.createUser); // POST /api/users/
-        this.put('/:id', ['ADMIN'], usersController.updateUser); // PUT /api/users/:id
+        this.put('/:id', ['ADMIN'] ,usersController.updateUser); // PUT /api/users/:id
         this.delete('/:id', ['ADMIN'], usersController.deleteUser); // DELETE /api/users/:id
 	}
 }
